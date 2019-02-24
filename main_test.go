058395a4a6dc36a2d11654a94801c4e9f66297c4
midnightconman <midnightconman@gmail.com>
2019-02-24 11:25:37 -0800

    Better Commands and Colors - Added some new flags for colors and output mode - Added a switch case for file vs command mode

--- main_test.go
+++ main_test.go
@@ -5,11 +5,13 @@ import (
 )
 
 func TestHandleSimple(t *testing.T) {
+	color := "ff2a6f78"
+	doneColor = &color
 	err := handle([]byte(`{"type":"TEST_DONE"}`))
 	if err != nil {
 		t.Errorf("%+v", err)
 	}
 	if currentEvent != "!Ybg0xff2a6f78Y!TEST_DONE" {
-		t.Error("handle output wasn't as expected.")
+		t.Errorf("handle output wasn't as expected. %+v", currentEvent)
 	}
 }
